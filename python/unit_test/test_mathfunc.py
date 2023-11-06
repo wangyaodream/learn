cfa2436552b7732d59848265b7627e3d107584a5
wangyao <wangyao.dream@hotmail.com>
2023-11-06 11:53:25 +0800

    add unittest demo

---
+++ python/unit_test/test_mathfunc.py
@@ -0,0 +1,27 @@
+import unittest
+from mathfunc import *
+
+
+class TestMathFunc(unittest.TestCase):
+    """Test mathfunc.py"""
+
+
+    def test_add(self):
+        """ Test method add(a, b)"""
+        self.assertEqual(3, add(1, 2))
+        self.assertNotEqual(3, add(2, 2))
+
+    def test_minus(self):
+        self.assertEqual(1, minus(3, 2))
+        self.assertNotEqual(3, minus(2, 2))
+
+    def test_multi(self):
+        self.assertEqual(9, multi(3, 3))
+
+    def test_divide(self):
+        self.assertEqual(10, divide(50, 5))
+        self.assertEqual(2.5, divide(5, 2))
+
+
+if __name__ == "__main__":
+    unittest.main()
