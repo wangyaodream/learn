ab7016e3f86a9cc2d24414f042dd6f04077280fc
wangyao <wangyao.dream@hotmail.com>
2024-01-04 11:57:19 +0800

    change standard

--- golang/standard/main.go
+++ golang/standard/main.go
@@ -3,10 +3,9 @@ package main
 
 import (
     "io"
-    "io/ioutil"
     "os"
     "log"
-    "fmt"
+    _ "fmt"
 )
 
 const (
@@ -17,21 +16,37 @@ const (
     e
 )
 
+var (
+    Trace   *log.Logger
+    Info    *log.Logger
+    Warning *log.Logger
+    Error   *log.Logger
+)
+
 func init() {
-    log.SetPrefix("TRACE:")
-    log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
+    // log.SetPrefix("TRACE:")
+    // log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
+    file, err := os.OpenFile("errors.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+    if err != nil {
+        log.Fatalln("Failed to open error log file:", err)
+    }
+
+    Trace = log.New(io.Discard, "TRACE: ", log.Ldate|log.Ltime|log.Lshortfile)
+    Info = log.New(io.Discard, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+    Warning = log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+    Error = log.New(io.MultiWriter(file, os.Stderr), "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 
 func main() {
-    log.Println("message")
 
-    log.Fatalln("fatal message")
 
     // log.Panicln("panic message")
 
-    fmt.Println(a)
-    fmt.Println(b)
-    fmt.Println(e)
+    Trace.Println("I have something standard to say.")
+    Info.Println("Specical information")
+    Warning.Println("There is something you need to know about")
+    Error.Println("Something has failed")
+
     
 }
