235e97c55e11ccb444e2cf65fca3b58da25c999f
wangyao <wangyao.dream@hotmail.com>
2024-03-01 22:14:01 +0800

    add gui

---
+++ golang/gui/dataBinding/conversion/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fyne.io/fyne/v2"
+    "fyne.io/fyne/v2/app"
+    "fyne.io/fyne/v2/container"
+    "fyne.io/fyne/v2/data/binding"
+    "fyne.io/fyne/v2/widget"
+)
+
+func main() {
+    myApp := app.New()
+    w := myApp.NewWindow("Conversion")
+
+    f := binding.NewFloat()
+    str := binding.FloatToString(f)
+    short := binding.FloatToStringWithFormat(f, "%0.0f%%")
+    f.Set(25.0)
+
+    w.SetContent(container.NewVBox(
+        widget.NewSliderWithData(0, 100.0, f),
+        widget.NewLabelWithData(str),
+        widget.NewLabelWithData(short),
+    ))
+
+    w.Resize(fyne.NewSize(300, 150))
+    w.ShowAndRun()
+}
+
