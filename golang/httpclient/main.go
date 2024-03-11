7382fa9fe8afe2c9c0aabba1afdd410a024f8313
wangyao <wangyao.dream@hotmail.com>
2024-03-11 22:49:25 +0800

    chapter httpclient completed

--- golang/httpclient/main.go
+++ golang/httpclient/main.go
@@ -9,6 +9,8 @@ import (
 	"time"
     // "strings"
     // "net/http/cookiejar"
+    "mime/multipart"
+    "bytes"
 )
 
 func main() {
@@ -97,8 +99,43 @@ func main() {
     //     }
     // }
 
-    req, err := http.NewRequest(http.MethodGet,
-    "http://localhost:5000/redirect1", nil)
+    // req, err := http.NewRequest(http.MethodGet,
+    // "http://localhost:5000/redirect1", nil)
+    // if (err == nil) {
+    //     var response *http.Response
+    //     response, err = http.DefaultClient.Do(req)
+    //     if (err == nil) {
+    //         io.Copy(os.Stdout, response.Body)
+    //     } else {
+    //         Printfln("Request Error: %v", err.Error())
+    //     }
+    // } else {
+    //     Printfln("Error: %v", err.Error())
+    // }
+    var buffer bytes.Buffer 
+    formWriter := multipart.NewWriter(&buffer)
+    fieldWriter, err := formWriter.CreateFormField("name")
+    if (err == nil) {
+        io.WriteString(fieldWriter, "Alice")
+    }
+    fieldWriter, err = formWriter.CreateFormField("city")
+    if (err == nil) {
+        io.WriteString(fieldWriter, "New York")
+    }
+    fileWriter, err := formWriter.CreateFormFile("codeFile", "printer.go")
+    if (err == nil) {
+        fileData, err := os.ReadFile("./printer.go")
+        if (err == nil) {
+            fileWriter.Write(fileData)
+        }
+    }
+
+    formWriter.Close()
+
+    req, err := http.NewRequest(http.MethodPost,
+    "http://localhost:5000/form", &buffer)
+    req.Header["Content-Type"] = []string{ formWriter.FormDataContentType()}
+
     if (err == nil) {
         var response *http.Response
         response, err = http.DefaultClient.Do(req)
