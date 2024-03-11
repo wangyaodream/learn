bd816efcf9fa8a143f3abbbae9c2f6b7bafee527
wangyao <wangyao.dream@hotmail.com>
2024-03-11 14:49:46 +0800

    add form post data

--- golang/httpclient/main.go
+++ golang/httpclient/main.go
@@ -2,41 +2,53 @@ package main
 
 import (
 	//"io"
-	// "encoding/json"
+	"encoding/json"
 	"io"
 	"net/http"
 	"os"
-
-	//"os"
 	"time"
+    "strings"
 )
 
 func main() {
     go http.ListenAndServe(":5000", nil)
     time.Sleep(time.Second)
 
-    formData := map[string][]string {
-        "name": {"Kayak"},
-        "category": {"watersports"},
-        "price": {"279"},
-    }
+    // formData := map[string][]string {
+    //     "name": {"Kayak"},
+    //     "category": {"watersports"},
+    //     "price": {"279"},
+    // }
 
     // response, err := http.Get("http://localhost:5000/json")
-    response, err := http.PostForm("http://localhost:5000/echo", formData)
-    if (err == nil && response.StatusCode == http.StatusOK) {
-        // defer response.Body.Close()
-        // data := []Product {}
-        // err = json.NewDecoder(response.Body).Decode(&data)
-        // if (err == nil) {
-        //     for _, p := range data {
-        //         Printfln("Name: %v, Price: $%.2f", p.Name, p.Price)
-        //     }
-        // } else {
-        //     Printfln("Decode error: %v", err.Error())
-        // }
-        io.Copy(os.Stdout, response.Body)
-        defer response.Body.Close()
+    // response, err := http.PostForm("http://localhost:5000/echo", formData)
+    // if (err == nil && response.StatusCode == http.StatusOK) {
+    //     // defer response.Body.Close()
+    //     // data := []Product {}
+    //     // err = json.NewDecoder(response.Body).Decode(&data)
+    //     // if (err == nil) {
+    //     //     for _, p := range data {
+    //     //         Printfln("Name: %v, Price: $%.2f", p.Name, p.Price)
+    //     //     }
+    //     // } else {
+    //     //     Printfln("Decode error: %v", err.Error())
+    //     // }
+    //     io.Copy(os.Stdout, response.Body)
+    //     defer response.Body.Close()
+    // } else {
+    //     Printfln("Error: %v, Status Code: %v", err.Error(), response.StatusCode)
+    // }
+    var builder strings.Builder
+    err := json.NewEncoder(&builder).Encode(Products[0])
+    if (err == nil) {
+        response, err := http.Post("http://localhost:5000/echo", "application/json",            strings.NewReader(builder.String()))
+        if (err == nil && response.StatusCode == http.StatusOK) {
+            io.Copy(os.Stdout, response.Body)
+            defer response.Body.Close()
+        } else {
+            Printfln("Error: %v", err.Error())
+        }
     } else {
-        Printfln("Error: %v, Status Code: %v", err.Error(), response.StatusCode)
+        Printfln("Error: %v", err.Error())
     }
 }
