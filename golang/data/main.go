452f306765fc96f49828caecee237d53f1eb2b6e
wangyao <wangyao.dream@hotmail.com>
2024-03-13 11:47:20 +0800

    add sql insert support

--- golang/data/main.go
+++ golang/data/main.go
@@ -14,38 +14,55 @@ type Product struct {
     Price float64
 }
 
-func queryDatabase(db *sql.DB) []Product {
-    products := []Product {}
-    rows, err := db.Query(`
+func queryDatabase(db *sql.DB, id int) (p Product) {
+    // products := []Product {}
+    // rows, err := db.Query(`
+    //     SELECT Products.Id, Products.Name, Products.Price,
+    //         Categories.Id as Cat_Id, Categories.Name as CatName
+    //         FROM Products, Categories
+    //     WHERE Products.Category = Categories.Id 
+    //         AND Categories.Name = ?`, categoryName)
+    row := db.QueryRow(`
         SELECT Products.Id, Products.Name, Products.Price,
             Categories.Id as Cat_Id, Categories.Name as CatName
             FROM Products, Categories
-        WHERE Products.Category = Categories.Id`)
-    if (err == nil) {
-        for (rows.Next()) {
-            p := Product{}
-            scanErr := rows.Scan(&p.Id, &p.Name, &p.Price, &p.Category.Id, &p.Category.Name)
-            if (scanErr == nil) {
-                products = append(products, p)
-            } else {
-                Printfln("Scan error: %v", scanErr)
-                break
-            }
+        WHERE Products.Category = Categories.Id 
+            AND Products.Id = ?`, id)
+    if (row.Err() == nil) {
+        scanErr := row.Scan(&p.Id, &p.Name, &p.Price,
+            &p.Category.Id, &p.Category.Name)
+        if (scanErr != nil) {
+            Printfln("Scan error: %v", scanErr)
         }
     } else {
-        Printfln("Error: %v", err)
+        Printfln("Row error: %v", row.Err().Error())
     }
-    return products
+    return
+}
+
+func insertRow(db *sql.DB, p *Product) (id int64) {
+    res, err := db.Exec(`
+        INSERT INTO Products (Name, Category, Price)
+        VALUES (?, ?, ?)`, p.Name, p.Category.Id, p.Price)
+    if (err == nil) {
+        id, err = res.LastInsertId()
+        if (err != nil) {
+            Printfln("Result error: %v", err.Error())
+        }
+    } else {
+        Printfln("Exec error: %v", err.Error())
+    }
+    return
 }
 
 func main() {
     // listDrivers()
     db, err := openDatabase()
     if (err == nil) {
-        products := queryDatabase(db)
-        for i, p := range products {
-            Printfln("#%v: %v", i, p)
-        }
+        newProduct := Product {Name: "Stadium", Category: Category{Id: 2}, Price: 79500}
+        newID := insertRow(db, &newProduct)
+        p := queryDatabase(db, int(newID))
+        Printfln("New Product: %v", p)
         db.Close()
     } else {
         panic(err)
