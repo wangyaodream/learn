01613822cf6ecb4ef21bcc4ac2ab5f1c61ae6077
wangyao <wangyao.dream@hotmail.com>
2024-03-13 23:12:04 +0800

    add reflect demo

---
+++ golang/reflection/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+    "reflect"
+    // "strings"
+    // "fmt"
+)
+
+func getTypePath(t reflect.Type) (path string) {
+    path = t.PkgPath()
+    if (path == "") {
+        path = "(built-in)"
+    }
+    return
+}
+
+// func printDetails(values ...interface{}) {
+//     for _, elem := range values {
+//         switch val := elem.(type) {
+//         case Product:
+//             Printfln("Product: Name: %v, Category: %v, Price: %v",
+//                 val.Name, val.Category, val.Price)
+//         case Customer:
+//             Printfln("Customer: Name: %v, City: %v", val.Name, val.City)
+//         }
+//     }
+// }
+
+// // 利用反射来支持多种不同类型的参数传入并处理
+// func printDetails(values ...interface{}) {
+//     for _, elem := range values {
+//         fieldDetails := []string {}
+//         elemType := reflect.TypeOf(elem)
+//         elemValue := reflect.ValueOf(elem)
+//         if elemType.Kind() == reflect.Struct {
+//             for i := 0; i < elemType.NumField(); i++ {
+//                 fieldName := elemType.Field(i).Name
+//                 fieldVal := elemValue.Field(i)
+//                 fieldDetails = append(fieldDetails, fmt.Sprintf("%v: %v", fieldName,fieldVal))
+//             }
+//             Printfln("%v(%v)", elemType.Name(), strings.Join(fieldDetails, ", "))
+//         } else {
+//             Printfln("%v: %v", elemType.Name(), elemValue)
+//         }
+//     }
+// }
+
+// // 使用Type的一些特性
+// func printDetails(values ...interface{}) {
+//     for _, elem := range values {
+//         elemType := reflect.TypeOf(elem)
+//         Printfln("Name: %v, PkgPath: %v, Kind: %v",
+//             elemType.Name(), getTypePath(elemType), elemType.Kind())
+//     }
+// }
+
+// 使用Value的一些特性
+func printDetails(values ...interface{}) {
+    for _, elem := range values {
+        elemValue := reflect.ValueOf(elem)
+        switch elemValue.Kind() {
+        case reflect.Bool:
+            var val bool = elemValue.Bool()
+            Printfln("Bool: %v", val)
+        case reflect.Int:
+            var val int64 = elemValue.Int()
+            Printfln("Int: %v", val)
+        case reflect.Float32, reflect.Float64:
+            var val float64 = elemValue.Float()
+            Printfln("Float: %v", val)
+        case reflect.String:
+            var val string = elemValue.String()
+            Printfln("String: %v", val)
+        case reflect.Ptr:
+            var val reflect.Value = elemValue.Elem()
+            if (val.Kind() == reflect.Int) {
+                Printfln("Pointer to Int: %v", val.Int())
+            }
+        default:
+            Printfln("Other: %v", elemValue.String())
+        }
+    }
+}
+
+type Payment struct {
+    Currency string
+    Amount float64
+}
+
+func main() {
+    product := Product {
+        Name: "Kayak", Category: "Watersports", Price: 279,
+    }
+    customer := Customer { Name: "Alice", City: "New York"}
+    // 可以使用不同的类型
+    // printDetails(product, customer)
+    payment := Payment { Currency: "USD", Amount: 100.6}
+    number := 100
+    printDetails(product, customer, payment, 11, true, &number, "Alice")
+}
+
---
+++ golang/reflection/types.go
@@ -0,0 +1,12 @@
+package main
+
+type Product struct {
+    Name, Category string
+    Price float64
+}
+
+type Customer struct {
+    Name, City string
+}
+
+
