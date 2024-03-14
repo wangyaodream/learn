70a782456419813df5c974b93c81688b5a257c76
wangyao <wangyao.dream@hotmail.com>
2024-03-14 23:30:58 +0800

    add converting and setting value

--- golang/reflection/main.go
+++ golang/reflection/main.go
@@ -1,10 +1,100 @@
 package main
 
 import (
-    "reflect"
-    // "strings"
-    // "fmt"
+	"reflect"
+	"strings"
+	// "fmt"
 )
+
+func IsInt(v reflect.Value) bool {
+    switch v.Kind() {
+        case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+            return true
+    }
+    return false
+}
+
+func IsFloat(v reflect.Value) bool {
+    switch v.Kind() {
+    case reflect.Float32, reflect.Float64:
+        return true
+    }
+    return false
+}
+
+func convert(src, target interface{}) (result interface{}, assigned bool) {
+    srcVal := reflect.ValueOf(src)
+    targetVal := reflect.ValueOf(target)
+    if (srcVal.Type().ConvertibleTo(targetVal.Type())) {
+        if (IsInt(targetVal) && IsInt(srcVal)) && targetVal.OverflowInt(srcVal.Int()) {
+            Printfln("Int overflow")
+            return src, false
+        } else if (IsFloat(targetVal) && IsFloat(srcVal) && targetVal.OverflowFloat(srcVal.Float())) {
+
+            Printfln("Float overflow")
+            return src, false
+        }
+        result = srcVal.Convert(targetVal.Type()).Interface()
+        assigned = true
+    } else {
+        result = src
+    }
+    return
+}
+
+func contains(slice interface{}, target interface{}) (found bool) {
+    sliceVal := reflect.ValueOf(slice)
+    targetType := reflect.TypeOf(target)
+    if (sliceVal.Kind() == reflect.Slice && sliceVal.Type().Elem().Comparable() &&
+        targetType.Comparable()) {
+        for i := 0; i < sliceVal.Len(); i++ {
+            if sliceVal.Index(i).Interface() == target {
+                found = true
+            }
+        }
+    }
+    return
+}
+
+func setAll(src interface{}, targets ...interface{}) {
+    srcVal := reflect.ValueOf(src)
+    for _, target := range targets {
+        targetVal := reflect.ValueOf(target)
+        if (targetVal.Kind() == reflect.Ptr &&
+                targetVal.Elem().Type() == srcVal.Type() &&
+                targetVal.Elem().CanSet()) {
+            targetVal.Elem().Set(srcVal)
+        }
+    }
+}
+
+func incrementOrUpper(values ...interface{}) {
+    for _, elem := range values {
+        elemValue := reflect.ValueOf(elem)
+        if (elemValue.Kind() == reflect.Ptr) {
+            elemValue = elemValue.Elem()
+        }
+        if (elemValue.CanSet()) {
+            switch (elemValue.Kind()) {
+                case reflect.Int:
+                    elemValue.SetInt(elemValue.Int() + 1)
+                case reflect.String:
+                    elemValue.SetString(strings.ToUpper(elemValue.String()))
+            }
+            Printfln("Modified Value: %v", elemValue)
+        } else {
+            Printfln("Cannot set %v: %v", elemValue.Kind(), elemValue)
+        }
+    }
+}
+
+func selectValue(data interface{}, index int) (result interface{}) {
+    dataVal := reflect.ValueOf(data)
+    if (dataVal.Kind() == reflect.Slice) {
+        result = dataVal.Index(index).Interface()
+    }
+    return
+}
 
 func getTypePath(t reflect.Type) (path string) {
     path = t.PkgPath()
@@ -88,14 +178,45 @@ type Payment struct {
 }
 
 func main() {
-    product := Product {
-        Name: "Kayak", Category: "Watersports", Price: 279,
-    }
-    customer := Customer { Name: "Alice", City: "New York"}
-    // 可以使用不同的类型
-    // printDetails(product, customer)
-    payment := Payment { Currency: "USD", Amount: 100.6}
-    number := 100
-    printDetails(product, customer, payment, 11, true, &number, "Alice")
+    // product := Product {
+    //     Name: "Kayak", Category: "Watersports", Price: 279,
+    // }
+    // customer := Customer { Name: "Alice", City: "New York"}
+    // // 可以使用不同的类型
+    // // printDetails(product, customer)
+    // payment := Payment { Currency: "USD", Amount: 100.6}
+    // number := 100
+    // printDetails(product, customer, payment, 11, true, &number, "Alice")
+    // names := []string {"Alice", "Bob", "Charlie"}
+    // scores := []interface{} {99, 88, 77}
+    // val := selectValue(scores, 1).(int)
+    // Printfln("Selected: %v", val)
+    // name := "Alice"
+    // price := 279
+    // city := "London"
+
+    // incrementOrUpper(&name, &price, &city)
+    // for _, val := range []interface{} { name, price, city } {
+    //     Printfln("Value: %v", val)
+    // }
+    //
+    // setAll("New String", &name, &price, &city)
+    // setAll(10, &name, &price, &city)
+    // for _, val := range []interface{} { name, price, city } {
+    //     Printfln("Value: %v", val)
+    // }
+
+    // Converting Values
+
+    name := "Alice"
+    price := 279
+
+    newVal, ok := convert(price, 100.00)
+    Printfln("Converted %v: %v, %T", ok, newVal, newVal)
+    newVal, ok = convert(name, 100.00)
+    Printfln("Converted %v: %v, %T", ok, newVal, newVal)
+
+    newVal, ok = convert(5000, int8(100))
+    Printfln("Converted %v: %v, %T", ok, newVal, newVal)
 }
 
