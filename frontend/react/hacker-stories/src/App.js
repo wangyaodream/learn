f866c39b126c3874476c5888147ce65a324a2a4c
wangyao <wangyao.dream@hotmail.com>
2024-04-01 20:12:29 +0800

    react pratice

--- frontend/react/hacker-stories/src/App.js
+++ frontend/react/hacker-stories/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+
+// const welcome = {
+//     greeting: 'Hey',
+//     title: 'React'
+// }
+
+function getTitle(title) {
+    return title;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div>
+            <h1>Hello, {getTitle('React')}</h1>
+
+            <label htmlFor='search'>Search: </label>
+            <input id='search' type="text" />
+        </div>
+    ); 
 }
 
 export default App;
