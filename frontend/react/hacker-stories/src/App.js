a3099283a6b7f1a29c9f46a713c05a413a901b12
wangyao <wangyao.dream@hotamil.com>
2024-04-02 10:47:36 +0800

    add winner

--- frontend/react/hacker-stories/src/App.js
+++ frontend/react/hacker-stories/src/App.js
@@ -1,30 +1,7 @@
 import * as React from 'react'
 import { useState } from 'react';
-// const welcome = {
-//     greeting: 'Hey',
-//     title: 'React'
-// }
 
 function Square({value, onSquareClick}) {
-    // const [value, setValue] = useState(null);
-    // function handleClick() {
-    //     // console.log("clicked!")
-    //     // if (value === "X") {
-    //     //     setValue('')
-    //     // } else {
-    //     //     setValue('X');
-    //     // }
-    //     setValue('X')
-    // }
-
-    // return (
-    //     <button
-    //     className='square'
-    //     onClick={handleClick}
-    //     >
-    //     {value}
-    //     </button>
-    // )
     return (
         <button 
             className='square' 
@@ -33,16 +10,56 @@ function Square({value, onSquareClick}) {
         </button>);
 }
 
-function App() {
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+function Board() {
+    const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
+    // 将胜利者显示出来
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner:" + winner;
+    } else {
+        status = "Next Player:" + (xIsNext ? "X" : "O");
+    }
+
     function handleClick(i) {
+        // 当所点击的块已经有值时就让它无法修改
+        if (squares[i] || calculateWinner(squares)) {
+            return;
+        }
         const nextSquares = squares.slice();
-        nextSquares[i] = 'X';
+        if (xIsNext) {
+            nextSquares[i] = 'X';
+        } else {
+            nextSquares[i] = 'O';
+        }
         setSquares(nextSquares);
+        setXIsNext(!xIsNext)
     }
     return (
         <>
+            <div className='staus'>{status}</div>
             <div className='board-row'>
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -62,4 +79,17 @@ function App() {
     ); 
 }
 
-export default App;
+function Game() {
+    return (
+        <div className='game'>
+            <div className='game-board'>
+                <Board></Board>
+            </div>
+            <div className='game-info'>
+                <ol>{}</ol>
+            </div>
+        </div>
+    )
+}
+
+export default Game;
