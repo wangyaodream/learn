fe7aab5be9d217b9c252c72acec884a8e4abc981
wangyao <wangyao.dream@hotmail.com>
2024-04-02 23:03:43 +0800

    add some api with fastapi

--- python/fastapi_app/main.py
+++ python/fastapi_app/main.py
@@ -1,9 +1,70 @@
+from enum import Enum
+from typing import Union
+
 from fastapi import FastAPI
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 app = FastAPI()
 
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"}
+]
 
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name is ModelName.lenet:
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some rediduals"}
+    
+
+# 查询参数
+# @app.get("/items/")
+# async def read_item(skip: int = 0, limit: int = 10):
+#     return fake_items_db[skip: skip + limit]
+
+
+# 可选参数
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: str | None = None):
+#     if q:
+#         return {"item_id": item_id, "q": q}
+#     return {"item_id": item_id}
+
+# convert type
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+#     item = {"item_id": item_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update(
+#             {"description": "this is an amazing item that has a long description"}
+#         )
+#     return item
+
+
+# 必选参数
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
