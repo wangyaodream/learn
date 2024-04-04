95d79635f1947883da63832005f85513c8573583
wangyao <wangyao.dream@hotmail.com>
2024-04-04 23:18:34 +0800

    add search to react app

--- frontend/react/my-app/src/App.js
+++ frontend/react/my-app/src/App.js
@@ -20,21 +20,22 @@ const Item = (props) => (
     </li>
 );
 
-const Search = () => {
-    const [searchTerm, setSearchTerm] = React.useState('');
-
-    const handleChange = (event) => {
-         setSearchTerm(event.target.value);
-    };
+const Search = (props) => {
+    // const [searchTerm, setSearchTerm] = React.useState('');
+    //
+    // const handleChange = (event) => {
+    //      setSearchTerm(event.target.value);
+    // };
+    const {search, onSearch} = props;
 
     return (
         <div>
             <label htmlFor='search'>Search: </label>
-            <input id='search' type='text' onChange={handleChange}/>
-
-            <p>
-                Searching for <strong>{searchTerm}</strong>
-            </p>
+            <input 
+                id='search' 
+                type='text' 
+                value={search}
+                onChange={onSearch}/>
         </div>
     )
 }
@@ -58,14 +59,25 @@ const App = () => {
             objectID:1,
         },
     ];
+
+    const [searchTerm, setSearchTerm] = React.useState("");
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    }
+
+    const searchedStories = stories.filter(function (story) {
+        return story.title.toLowerCase().includes(searchTerm.toLowerCase());
+    })
+
     return (
         <div>
             <h1>My Hacker Stories</h1>
 
-            <Search />
+            <Search search={searchTerm} onSearch={handleSearch}/>
 
             <hr />
-            <List list={stories}/>
+            <List list={searchedStories}/>
         </div>
     )
 }
