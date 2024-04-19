efeb1d51133ead18bd2a462ffb618a9137cbb55a
wangyao <wangyao.dream@hotmail.com>
2024-04-19 23:41:33 +0800

    add frame and use classes

--- python/gui/customtkinter_learn/main.py
+++ python/gui/customtkinter_learn/main.py
@@ -1,23 +1,30 @@
 import customtkinter
 
+class MyCheckboxFrame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="check 1")
+        self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10,0), sticky="w")
+        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="check 2")
+        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10,0), sticky="w")
+        
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
         self.title("my app")
-        self.geometry("400x150")
-        self.grid_columnconfigure((0, 1), weight=1)
+        self.geometry("400x180")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
         # fill widget
-        self.button = customtkinter.CTkButton(self,
-                                              text="my button",
-                                              command=self.button_callback)
-        self.button.grid(row=0, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
-        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="check 1")
-        self.checkbox_1.grid(row=1, column=0, padx=20, pady=(0,20), sticky="w")
-        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="check 2")
-        self.checkbox_2.grid(row=1, column=1, padx=20, pady=(0,20), sticky="w")
+        self.checkbox_frame = MyCheckboxFrame(self)
+        self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsw")
+
+        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
+        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
 
     
     def button_callback(self):
