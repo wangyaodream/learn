4103c1dd82702dd07d49edf9f5e367a53d1bb524
wangyao <wangyao.dream@hotmail.com>
2024-04-20 00:21:49 +0800

    add radiobutton frame

--- python/gui/customtkinter_learn/main.py
+++ python/gui/customtkinter_learn/main.py
@@ -1,34 +1,79 @@
 import customtkinter
 
 class MyCheckboxFrame(customtkinter.CTkFrame):
-    def __init__(self, master):
+    def __init__(self, master, title, values):
         super().__init__(master)
+        self.grid_columnconfigure(0, weight=1)
+        self.values = values
+        self.title = title
+        self.checkboxes = []
 
-        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="check 1")
-        self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10,0), sticky="w")
-        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="check 2")
-        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10,0), sticky="w")
+        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="gray30", corner_radius=6)
+        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
+
+        for i, value in enumerate(self.values):
+            checkbox = customtkinter.CTkCheckBox(self, text=value)
+            checkbox.grid(row=i + 1, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.checkboxes.append(checkbox)
         
+    def get(self):
+        checked_checkboxes = []
+        for checkbox in self.checkboxes:
+            if checkbox.get() == 1:
+                checked_checkboxes.append(checkbox.cget("text"))
+        return checked_checkboxes
+
+
+class MyRadiobuttonFrame(customtkinter.CTkFrame):
+    def __init__(self, master, title, values):
+        super().__init__(master)
+        self.grid_columnconfigure(0, weight=1)
+        self.values = values
+        self.title = title
+        self.checkboxes = []
+        self.variable = customtkinter.StringVar(value="")
+
+        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="gray30", corner_radius=6)
+        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
+
+        for i, value in enumerate(self.values):
+            radiobutton = customtkinter.CTkRadioButton(self, text=value, value=value, variable=self.variable)
+            radiobutton.grid(row=i + 1, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.checkboxes.append(radiobutton)
+
+        
+    def get(self):
+        return self.variable.get()
+
+    # def set(self, value):
+    #     self.variable.set(value)
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
         self.title("my app")
-        self.geometry("400x180")
-        self.grid_columnconfigure(0, weight=1)
+        self.geometry("400x200")
+        self.grid_columnconfigure((0, 1), weight=1)
         self.grid_rowconfigure(0, weight=1)
 
         # fill widget
-        self.checkbox_frame = MyCheckboxFrame(self)
-        self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsw")
+        self.checkbox_frame = MyCheckboxFrame(self, "Values", values=["value 1", "value 2", "value 3"])
+        self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
+        self.radiobutton_frame =MyRadiobuttonFrame(self, "Options", values=["option 1", "option 2"])
+        self.radiobutton_frame.grid(row=0, column=1, padx=10, pady=(10, 0), sticky="nsew")
 
+        self.radiobutton_frame.configure(fg_color="transparent")
+
+        # 使用Frame之后，在Frame内的widget布局变动不再影响外部的，所以button的并不需要修改
         self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
-        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
+        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
 
     
     def button_callback(self):
-        print("button pressed")
+        print("checked checkboxes 1:", self.checkbox_frame.get())
+        print("checked checkboxes 2:", self.radiobutton_frame.get())
+
 
 
 if __name__ == "__main__":
