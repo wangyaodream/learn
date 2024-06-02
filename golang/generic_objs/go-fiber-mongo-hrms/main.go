3a68d63f5de35bde5238bc3a459c62c33c8b1692
wangyao <wangyao.dream@hotmail.com>
2024-06-02 23:13:22 +0800

    add new project go-fiber-mongo-hrms

---
+++ golang/generic_objs/go-fiber-mongo-hrms/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v3"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoInstance struct {
+	Client
+	Db
+}
+
+var mg MongoInstance
+
+const dbName = "fiber-hrms"
+const mongoURI = "mongodb://localhost:27017" + dbName
+
+type Employee struct {
+	ID     string
+	Name   string
+	Salary float64
+	Age    float64
+}
+
+func Connect() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+
+}
+
+func main() {
+	if err := Connect(); err != nil {
+		log.Fatal(err)
+	}
+
+	app := fiber.New()
+
+	app.Get("/employee", func(c fiber.Ctx) error {
+	})
+	app.Post("/employee")
+	app.Put("/employee/:id")
+	app.Delete("/employee/id")
+
+	log.Fatal(app.Listen(":3000"))
+}
