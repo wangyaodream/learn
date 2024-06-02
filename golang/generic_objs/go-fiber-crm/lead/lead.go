09f017e548e32f6d88d4dffac6aead0925e52e4a
wangyao <wangyao.dream@hotmail.com>
2024-06-02 22:01:54 +0800

    add functions delete and new

--- golang/generic_objs/go-fiber-crm/lead/lead.go
+++ golang/generic_objs/go-fiber-crm/lead/lead.go
@@ -30,10 +30,27 @@ func GetLead(c *fiber.Ctx) {
 	c.JSON(lead)
 }
 
-func NewLead() {
-
+func NewLead(c *fiber.Ctx) {
+	db := database.DBConn
+	lead := new(Lead)
+	if err := c.BodyParser(lead); err != nil {
+		c.Status(503).Send(err)
+		return
+	}
+	db.Create(&lead)
+	c.JSON(lead)
 }
 
-func DeleteLead() {
+func DeleteLead(c *fiber.Ctx) {
+	id := c.Params("id")
+	db := database.DBConn
 
+	var lead Lead
+	db.First(&lead, id)
+	if lead.Name == "" {
+		c.Status(500).Send("No lead found with ID")
+		return
+	}
+	db.Delete(&lead)
+	c.Send("Lead successfully Deleted")
 }
