32e251d09eb562e3742540d12a0e07eb3bad84a9
wangyao <wangyao.dream@hotmail.com>
2024-07-10 23:11:02 +0800

    performance test

--- python/generic/concurrent/demo.py
+++ python/generic/concurrent/demo.py
@@ -1,20 +1,26 @@
 import asyncio
 import random
+from time import perf_counter
 
 
 async def process(num: int):
-    await asyncio.sleep(3)
+    await asyncio.sleep(1)
     return f"process-{num}"
 
 
 async def main():
+    time_before = perf_counter()
     for _ in range(4):
         r = await process(random.randint(1, 999))
         print(r)
+    print(f"Total time (synchronous): {perf_counter() - time_before}")
 
+    
+    time_before = perf_counter()
     # 作为对比，使用gather后只需要3秒
     result = await asyncio.gather(*[process(random.randint(1, 999)) for _ in range(4)])
     print(result)
+    print(f"Total time (asynchronous): {perf_counter() - time_before}")
 
 
 if __name__ == "__main__":
