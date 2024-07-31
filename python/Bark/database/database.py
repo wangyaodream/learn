5a75dd74dbe951b6f5ae84656fb7e9014381906f
wangyao <wangyao.dream@hotmail.com>
2024-07-31 23:48:31 +0800

    add commands

--- python/Bark/database/database.py
+++ python/Bark/database/database.py
@@ -1,4 +1,8 @@
+import sys
 import sqlite3
+from datetime import datetime
+
+
 
 
 class DatabaseManager:
@@ -70,3 +74,42 @@ class DatabaseManager:
             tuple(criteria.values())
         )
 
+
+db = DatabaseManager('bookmarks.db')
+
+class CreateBookmarksTableCommand:
+    def execute(self):
+
+        db.create_table('bookmarks', {
+            'id': 'integer primary key autoincrement',
+            'title': 'text not null',
+            'url': 'text not null',
+            'notes': 'text',
+            'date_added': 'text not null'
+        })
+
+
+class AddBookmarkCommand:
+    def execute(self, data):
+        data['date_added'] = datetime.utcnow().isoformat()
+        db.add('bookmarks', data)
+        return 'Bookmark added!'
+
+
+class ListBookmarksCommand:
+    def __init__(self, order_by='date_added'):
+        self.order_by = order_by
+
+    def execute(self):
+        return db.select('bookmarks', order_by=self.order_by).fetchall()
+
+
+class DeleteBookmarkCommand:
+    def execute(self, data):
+        db.delete('bookmarks', {'id': data})
+        return 'Bookmark deleted!'
+
+
+class QuitCommand:
+    def execute(self):
+        sys.exit()
