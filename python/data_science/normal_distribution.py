6b32eca0563bf3e6d4af20fa1db23efa10776128
wangyao <wangyao.dream@hotmail.com>
2024-08-01 16:18:57 +0800

    improve the normal distribution

--- python/data_science/normal_distribution.py
+++ python/data_science/normal_distribution.py
@@ -1,11 +1,22 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 np.random.seed(0)
 sample_size = 10000
-sample = np.array([np.random.binomial(1, 0.5) for _ in range(sample_size)])
-head_count = sample.sum()
-head_count_frequency = head_count / sample_size
+# sample = np.array([np.random.binomial(1, 0.5) for _ in range(sample_size)])
+# head_count = sample.sum()
+# head_count_frequency = head_count / sample_size
 
-print(head_count_frequency)
-print(sample[:10])
+frequencies = np.random.binomial(sample_size, 0.5, 100000) / sample_size
+
+sample_means = frequencies
+likelihoods, bin_edges, _ = plt.hist(sample_means, bins='auto', edgecolor='black', density=True)
+
+plt.xlabel('Binned Sample Mean')
+plt.ylabel('Relative Likelihood')
+plt.show()
+
+# 计算均值和标准差
+mean_normal = np.average(bin_edges[:-1], weights=likelihoods)
+var_normal = 
