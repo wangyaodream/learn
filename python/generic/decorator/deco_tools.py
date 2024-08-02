4fc90cacba78057c5d24f2a6aac32e9ab875efab
wangyao <wangyao.dream@hotmail.com>
2024-08-02 23:56:24 +0800

    add decorator relay and memoize

--- python/generic/decorator/deco_tools.py
+++ python/generic/decorator/deco_tools.py
@@ -21,6 +21,48 @@ def debugger(func):
     return wrapper
 
 
+def memoize(func):
+    cache = {}
+    def wrapper(*args):
+        if args in cache:
+            return cache[args]
+        else:
+            result = func(*args)
+            cache[args] = result
+            return result
+    return wrapper
+
+
+def retry(max_attepts, delay=1):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            attempts = 0
+            while attempts < max_attepts:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    attempts += 1
+                    print(f"Attempt {attempts} failed: {e}")
+                    time.sleep(delay)
+            print(f"Function failed after {max_attepts} attempts")
+        return wrapper
+    return decorator
+
+
+@retry(max_attepts=3, delay=2)
+def fetch_data(url):
+    print("Fetching the data...")
+    raise TimeoutError("Server is not responding")
+
+
+@memoize
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
 @debugger
 def add_number(x, y):
     return x + y
@@ -35,4 +77,8 @@ def train_model():
 
 if __name__ == "__main__":
     # train_model()
-    add_number(10, y=29)
+    # add_number(10, y=29)
+    # r = fibonacci(19)
+    # print(r)
+    fetch_data("https://www.google.com")
+
