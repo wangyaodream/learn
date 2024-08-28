82712341c33149c09c23eebfb202c4002d48b437
wangyao <wangyao.dream@hotmail.com>
2024-08-28 23:59:09 +0800

    format server code

--- golang/practices/grpc/binddata-streaming/server/server.go
+++ golang/practices/grpc/binddata-streaming/server/server.go
@@ -14,72 +14,72 @@ import (
 )
 
 type repoService struct {
-    pb.UnimplementedRepoServer
+	pb.UnimplementedRepoServer
 }
 
 // 实现CreateRepo方法
 func (s *repoService) CreateRepo(stream pb.Repo_CreateRepoServer) error {
-    var repoContext *pb.RepoContext
-    var data []byte
+	var repoContext *pb.RepoContext
+	var data []byte
 
-    for {
-        r, err := stream.Recv()
-        if err == io.EOF {
-            break
-        }
+	for {
+		r, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
 
-        switch t := r.Body.(type) {
-        case *pb.RepoCreateRequest_Context:
-            repoContext = r.GetContext()
-        case *pb.RepoCreateRequest_Data:
-            b := r.GetData()
-            data = append(data, b...)
-        case nil:
-            return status.Errorf(
-                codes.InvalidArgument,
-                "Message doesn't contain contedxt or data",
-            )
-        default:
-            return status.Errorf(
-                codes.FailedPrecondition,
-                "Unexpected message type: %s",
-                t,
-            )
-        }
-    }
+		switch t := r.Body.(type) {
+		case *pb.RepoCreateRequest_Context:
+			repoContext = r.GetContext()
+		case *pb.RepoCreateRequest_Data:
+			b := r.GetData()
+			data = append(data, b...)
+		case nil:
+			return status.Errorf(
+				codes.InvalidArgument,
+				"Message doesn't contain contedxt or data",
+			)
+		default:
+			return status.Errorf(
+				codes.FailedPrecondition,
+				"Unexpected message type: %s",
+				t,
+			)
+		}
+	}
 
-    // 创建响应消息
-    repo := pb.Repository{
-        Name: repoContext.Name,
-        Url: fmt.Sprintf("https://git.example.com/%s/%s", repoContext.CreatorId, repoContext.Name),
-    }
-    r := pb.RepoCreateReply{
-        Repo: &repo,
-        Size: int32(len(data)),
-    }
-    return stream.SendAndClose(&r)
+	// 创建响应消息
+	repo := pb.Repository{
+		Name: repoContext.Name,
+		Url:  fmt.Sprintf("https://git.example.com/%s/%s", repoContext.CreatorId, repoContext.Name),
+	}
+	r := pb.RepoCreateReply{
+		Repo: &repo,
+		Size: int32(len(data)),
+	}
+	return stream.SendAndClose(&r)
 }
 
 func registerServices(s *grpc.Server) {
-    pb.RegisterRepoServer(s, &repoService{})
+	pb.RegisterRepoServer(s, &repoService{})
 }
 
 func startServer(s *grpc.Server, l net.Listener) error {
-    return s.Serve(l)
+	return s.Serve(l)
 }
 
 func main() {
-    listenAddr := os.Getenv("LISTEN_ADDR")
-    if len(listenAddr) == 0 {
-        listenAddr = ":5000"
-    }
+	listenAddr := os.Getenv("LISTEN_ADDR")
+	if len(listenAddr) == 0 {
+		listenAddr = ":5000"
+	}
 
-    lis, err := net.Listen("tcp", listenAddr)
-    if err != nil {
-        log.Fatal(err)
-    }
+	lis, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    s := grpc.NewServer()
-    registerServices(s)
-    log.Fatal(startServer(s, lis))
+	s := grpc.NewServer()
+	registerServices(s)
+	log.Fatal(startServer(s, lis))
 }
