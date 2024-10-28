cdbfc210a7b6e13e233af670b739ee43bd4f99fd
wangyaodream <wangyao.dream@hotmail.com>
2024-10-28 17:28:50 +0800

    skyblitz update lost judge

--- python/game/skyblitz/main.py
+++ python/game/skyblitz/main.py
@@ -23,13 +23,20 @@ PLAYER_VEL = 5
 
 FONT = pygame.font.SysFont("comicsans", 30)
 
-def draw(player, elapsed_time):
+STAR_WIDTH = 10
+STAR_HEIGHT = 20
+STAR_VEL = 3
+
+def draw(player, elapsed_time, stars):
     WIN.blit(BG, (0, 0))
 
     time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
     WIN.blit(time_text, (10, 10))
 
     pygame.draw.rect(WIN, (255, 0, 0), player)
+
+    for star in stars:
+        pygame.draw.rect(WIN, "white", star)
 
     pygame.display.update()
 
@@ -42,10 +49,26 @@ def main():
 
     start_time = time.time()
     elapsed_time = 0
+
+    star_add_increment = 2000
+    star_count = 0
+
+    stars = []
+    hit = False
+
     while run:
         # FPS
-        clock.tick(FPS)
+        star_count += clock.tick(FPS)
         elapsed_time = time.time() - start_time
+
+        if star_count > star_add_increment:
+            # 生成3个star
+            for _ in range(3):
+                star_x = random.randint(0, WIDTH - STAR_WIDTH)
+                star = pygame.Rect(star_x, -STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)
+                stars.append(star)
+            star_add_increment = max(200, star_add_increment - 50)
+            star_count = 0
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -57,8 +80,23 @@ def main():
             player.x -= PLAYER_VEL
         elif keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width <= WIDTH:
             player.x += PLAYER_VEL
-        draw(player, elapsed_time)
 
+        for star in stars[:]:
+            star.y += STAR_VEL
+            if star.y > HEIGHT:
+                stars.remove(star)
+            elif star.y + star.height >= player.y and star.colliderect(player):
+                stars.remove(star)
+                hit = True
+                break
+
+        if hit:
+            lost_text = FONT.render("You Lost!", 1, "white")
+            WIN.blit(lost_text, (WIDTH/2 - lost_text.get_width()/2, HEIGHT/2 - lost_text.get_height()/2))
+            pygame.display.update()
+            pygame.time.delay(4000)
+            break
+        draw(player, elapsed_time, stars)
     pygame.quit()
 
 
