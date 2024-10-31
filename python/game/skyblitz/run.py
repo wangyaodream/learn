e3b4e5ca06f86483ef3d5ae4bb0830b6af2383e5
wangyao <wangyao.dream@hotmail.com>
2024-10-31 23:30:38 +0800

    spaceship movement

--- python/game/skyblitz/run.py
+++ python/game/skyblitz/run.py
@@ -8,6 +8,7 @@ WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 FPS = 60
+VEL = 5
 
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
@@ -27,6 +28,28 @@ def draw_window(red, yellow):
     pygame.display.update()
 
 
+def red_handle_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_a]:
+        red.x -= VEL
+    if keys_pressed[pygame.K_d]:
+        red.x += VEL
+    if keys_pressed[pygame.K_w]:
+        red.y -= VEL
+    if keys_pressed[pygame.K_s]:
+        red.y += VEL
+
+
+def yellow_handle_movement(keys_pressed, yellow):
+    if keys_pressed[pygame.K_LEFT]:
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_RIGHT]:
+        yellow.x += VEL
+    if keys_pressed[pygame.K_UP]:
+        yellow.y -= VEL
+    if keys_pressed[pygame.K_DOWN]:
+        yellow.y += VEL
+
+
 def main():
     red = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
     yellow = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
@@ -38,7 +61,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        red.x += 1
+        keys_pressed = pygame.key.get_pressed()
+        red_handle_movement(keys_pressed, red)
+        yellow_handle_movement(keys_pressed, yellow)
         draw_window(red, yellow)
 
     pygame.quit()
