b4f447d0933bd24b82b81002d50c7f3f8ebf21de
wangyao <wangyao.dream@hotmail.com>
2024-10-31 23:53:43 +0800

    set a border in skyblitz's window

--- python/game/skyblitz/run.py
+++ python/game/skyblitz/run.py
@@ -7,6 +7,10 @@ pygame.display.set_caption("skyblitz pro")
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
+BLACK = (0, 0, 0)
+
+BORDER = pygame.Rect(WIDTH/2 - 5, 0, 10, HEIGHT)
+
 FPS = 60
 VEL = 5
 
@@ -22,6 +26,7 @@ RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMA
 def draw_window(red, yellow):
     """绘制的顺序会相互覆盖"""
     WIN.fill((255,255,255))
+    pygame.draw.rect(WIN, BLACK, BORDER)
 
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
@@ -29,18 +34,18 @@ def draw_window(red, yellow):
 
 
 def red_handle_movement(keys_pressed, red):
-    if keys_pressed[pygame.K_a]:
+    if keys_pressed[pygame.K_a] and red.x - VEL > 0:
         red.x -= VEL
-    if keys_pressed[pygame.K_d]:
+    if keys_pressed[pygame.K_d] and red.x + VEL + red.width < BORDER.x:
         red.x += VEL
-    if keys_pressed[pygame.K_w]:
+    if keys_pressed[pygame.K_w] and red.y - VEL > 0:
         red.y -= VEL
-    if keys_pressed[pygame.K_s]:
+    if keys_pressed[pygame.K_s] and red.y + VEL + red.height < HEIGHT - 15:
         red.y += VEL
 
 
 def yellow_handle_movement(keys_pressed, yellow):
-    if keys_pressed[pygame.K_LEFT]:
+    if keys_pressed[pygame.K_LEFT] and yellow.x - VEL - yellow.width > BORDER.x - 15:
         yellow.x -= VEL
     if keys_pressed[pygame.K_RIGHT]:
         yellow.x += VEL
@@ -48,6 +53,10 @@ def yellow_handle_movement(keys_pressed, yellow):
         yellow.y -= VEL
     if keys_pressed[pygame.K_DOWN]:
         yellow.y += VEL
+
+
+def handle_movement(keys_pressed, red, yellow):
+    pass
 
 
 def main():
