471b7ed60b63171d6211ebfacf4f157ec631a12f
wangyao <wangyao.dream@hotmail.com>
2024-11-25 22:16:26 +0800

    add frontend demo with react

---
+++ python/farm/react/my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Welcome from './Welcome';
+
+function App() {
+    return (
+        <div>
+            <Welcome name="Sara" />
+            <Welcome name="Cahal" />
+            <Welcome name="Edite" />
+        </div>
+    );
+}
+
+export default App;
