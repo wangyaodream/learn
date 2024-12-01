3b7ebc1d59a759f7e20f5c699bfe09eeb8fcbebf
wangyao <wangyao.dream@hotmail.com>
2024-12-01 22:12:45 +0800

    ListToDoLists component completed

--- python/farm/frontend/src/ListToDoLists.js
+++ python/farm/frontend/src/ListToDoLists.js
@@ -14,7 +14,64 @@ function ListToDoLists({
         return <div className="ListToDoLists loading">Loading... to-do lists</div>;
     } else if (listSummaries.length === 0) {
         return (
-            div
+            <div className="ListToDoLists">
+                <div className="box">
+                    <label>
+                        New To-Do List:&nbsp;
+                        <input id={labelRef} type="text" />
+                    </label>
+                    <button 
+                        onClick={() =>
+                            handleNewToDoList(document.getElementById(labelRef).value)
+                        }
+                    >
+                        New
+                    </button>
+                </div>
+                <p>There are no to-do lists.</p>
+            </div>
         );
     }
+    return (
+        <div className="ListToDoLists">
+            <h1>All To-Do Lists</h1>
+            <div className="box">
+                <label>
+                    New To-Do List:&nbsp;
+                    <input id={labelRef} type="text" />
+                </label>
+                <button 
+                    onClick={() =>
+                        handleNewToDoList(document.getElementById(labelRef).value)
+                    }
+                >
+                    New
+                </button>
+            </div>
+            {listSummaries.map((summary) => {
+                return (
+                    <div
+                        key={summary.id}
+                        className="summary"
+                        onClick={() => handleSelectList(summary.id)}
+                    >
+                        <span className="name">{summary.name}</span>
+                        <span className="count">({summary.item_count} items)</span>
+                        <span className="flex"></span>
+                        <span
+                            className="trash"
+                            onClick={(evt) => {
+                                evt.stopPropagation();
+                                handleDeleteToDoList(summary.id);
+                            }}
+                        >
+                        <BiSolidTrash />
+                        </span>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
+
+export default ListToDoLists;
